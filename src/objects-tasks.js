2974cb22af269ee78779403e54598ccfc315fe73
kozochkina82 <kozochkina82@yandex.ru>
2025-12-16 17:15:36 +0100

    feat: add answer

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,10 +33,19 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const merged = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (merged[key]) {
+        merged[key] += value;
+      } else {
+        merged[key] = value;
+      }
+    });
+  });
+  return merged;
 }
-
 /**
  * Removes a properties from an object.
  *
